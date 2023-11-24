1d8b4be037d90cbd74aed0d4778d455612fd2a34
patrick tomas <p.tomas14@gmail.com>
2023-11-24 15:17:54 -0800

    add google api client

---
+++ public/homePage.js
@@ -0,0 +1,21 @@
+var feedUrl = "http://www.google.com/calender/feeds/default/public/full";
+
+function setUpMyService() {
+    var myService = new google.gdata.calendar.CalendarService('habitual-buy-1')
+    return myService;
+}
+
+function getMyFeed() {
+    myService = setUpMyService();
+
+    myService.getEventsFeed(feedUrl, handleMyFeed, handleError);
+}
+
+function handleMyFeed(myResultsFeedRoot) {
+    alert("This feeds title is:" + myResultsFeedRoot)
+}
+
+function handleError(e) {
+    alert('there was an error');
+    alert(e.cause ? e.cause.statusText : e.message);
+}
